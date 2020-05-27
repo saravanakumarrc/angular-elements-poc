4660acba953bda37b4a4a3795617ac9cac2d0047
Saravanakumar Radhakrishnan <saravanakumar_radhakrishnan@trimble.com>
2020-05-28 00:06:46 +0530

    tire component working state

--- src/app/ams-ng-tire/ams-ng-tire.component.ts
+++ src/app/ams-ng-tire/ams-ng-tire.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, ViewEncapsulation, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, OnInit } from '@angular/core';
 import { PositionConfiguration } from './position-configuration.model';
 
 @Component({
@@ -7,7 +7,6 @@ import { PositionConfiguration } from './position-configuration.model';
   styleUrls: ['./ams-ng-tire.component.scss']
 })
 export class TireComponent implements OnInit, OnChanges {
-  @Input() mode = 'viewer';
   @Input() positionConfiguration: PositionConfiguration;
   @Output() select = new EventEmitter();
   serialNumber = '';
@@ -15,51 +14,15 @@ export class TireComponent implements OnInit, OnChanges {
   constructor() { }
 
   ngOnInit() {
-    console.log('TireComponent:mode', this.mode);
     console.log('TireComponent:positionConfiguration', this.positionConfiguration);
   }
 
   ngOnChanges() {
-    console.log('TireComponent:ngOnChange:mode:', this.mode);
     console.log('TireComponent:ngOnChange:positionConfiguration:', this.positionConfiguration);
-    this.serialNumber = this.positionConfiguration.unitTireInfo != null && this.positionConfiguration.unitTireInfo.serialNumber !== '' ?
-                        ('Part Number: ' + this.positionConfiguration.unitTireInfo.partNumber
-                          + '  Serial Number ' + this.positionConfiguration.unitTireInfo.serialNumber) : 'No Serial';
   }
 
   onClick(): void {
     console.log('TireComponent:onClick', this.positionConfiguration);
-    
     this.select.emit(this.positionConfiguration);
-
-    this.positionConfiguration.selected = !this.positionConfiguration.selected;
-
-    if (this.positionConfiguration.selected && this.positionConfiguration.isInvalid) {
-      this.positionConfiguration.isInvalid = false;
-    } else if (!this.positionConfiguration.selected && this.positionConfiguration.isInvalid === false) {
-      if (!this.positionConfiguration.positionId) {
-        this.positionConfiguration.isInvalid = true;
-      } else {
-        this.positionConfiguration.isInvalid = false;
-      }
-    }
-  }
-
-  IsEmpty() {
-    if (this.positionConfiguration != null
-        && this.positionConfiguration.unitTireInfo != null
-        && this.positionConfiguration.unitTireInfo.partNumber === '') {
-          return true;
-    }
-    return false;
-  }
-
-  IsSpare() {
-    return this.positionConfiguration.sequence === 0;
-  }
-
-  get SerialNumber() {
-    return this.positionConfiguration.unitTireInfo != null && this.positionConfiguration.unitTireInfo.serialNumber !== '' ?
-      this.positionConfiguration.unitTireInfo.serialNumber : '';
   }
 }
